508b99fde00e531f8a0a30d7a2b11023065ba29b
John Knotts <jhnnyk@gmail.com>
2017-04-14 11:22:22 -0600

    moving fillups to their own controller for clarity

---
+++ app/controllers/fillup_controller.rb
@@ -0,0 +1,32 @@
+class FillupController < ApplicationController
+
+  get '/cars/:id/fillups/new' do
+    if logged_in?
+      @car = Car.find_by(:id => params[:id])
+      erb :'fillups/new'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/fillups/:id' do
+    mileage  = Sanitize.fragment(params[:mileage])
+    gallons  = Sanitize.fragment(params[:gallons])
+    octane   = Sanitize.fragment(params[:octane])
+    price    = Sanitize.fragment(params[:price])
+    brand    = Sanitize.fragment(params[:brand])
+    location = Sanitize.fragment(params[:location])
+
+    @car = Car.find_by(:id => params[:id])
+    @fillup = Fillup.create(:mileage => mileage,
+                            :gallons => gallons,
+                            :octane  => octane,
+                            :price   => price,
+                            :car_id  => @car.id)
+    @fillup.fillup_date = Time.now
+    @fillup.save
+
+    redirect to "/cars/#{@car.id}"
+  end
+  
+end
---
+++ spec/controllers/fillup_controller_spec.rb
@@ -0,0 +1,84 @@
+require 'spec_helper'
+
+describe FillupController do
+
+  describe 'new fillup' do
+    context 'logged in' do
+      it 'lets user view new fillup form if logged in' do
+        user = User.create(:username => "becky567", :email => "starz@aol.com", :password => "kittens")
+        car = Car.create(:year => 2000, :car_make => "Toyota", :car_model => "Land Cruiser", :nickname => "Land Cruiser", :mileage => 318150, :user_id => user.id)
+
+        visit '/login'
+
+        fill_in(:username, :with => "becky567")
+        fill_in(:password, :with => "kittens")
+        click_button 'Login'
+
+        visit "/cars/#{car.id}/fillups/new"
+        expect(page.status_code).to eq(200)
+      end
+
+      it 'lets user create a fillup if they are logged in' do
+        user = User.create(:username => "becky567", :email => "starz@aol.com", :password => "kittens")
+        car = Car.create(:year => 2000, :car_make => "Toyota", :car_model => "Land Cruiser", :nickname => "Land Cruiser", :mileage => 318150, :user_id => user.id)
+
+        visit '/login'
+
+        fill_in(:username, :with => "becky567")
+        fill_in(:password, :with => "kittens")
+        click_button 'Login'
+
+        visit "/cars/#{car.id}/fillups/new"
+
+        fill_in(:mileage, :with => 1999)
+        fill_in(:gallons, :with => 21.158)
+        fill_in(:price, :with => 60.22)
+        click_button 'Add fill up'
+
+        expect(page.body).to include("21.158")
+      end
+    end
+
+    context 'logged out' do
+      it 'does not let user view new fillup form if not logged in' do
+        get '/cars/1/fillups/new'
+        expect(last_response.location).to include("/login")
+      end
+    end
+  end
+
+  describe 'show action' do
+    context 'logged in' do
+      it 'displays a single fillup' do
+
+        user = User.create(:username => "becky567", :email => "starz@aol.com", :password => "kittens")
+        car = Car.create(:year => 2000, :car_make => "Toyota", :car_model => "Land Cruiser", :nickname => "Land Cruiser", :mileage => 318150, :user_id => user.id)
+        fillup = Fillup.create(:mileage => 215000, :gallons => 12, :octane => "91", :price => 36.00, :brand => "Chevron", :location => "Evergreen, CO", :car_id => car.id, :fillup_date => Time.now)
+
+        visit '/login'
+
+        fill_in(:username, :with => "becky567")
+        fill_in(:password, :with => "kittens")
+        click_button 'Login'
+
+        visit "/fillups/#{fillup.id}"
+        expect(page.status_code).to eq(200)
+        expect(page.body).to include("Delete Fillup")
+        expect(page.body).to include(fillup.location)
+        expect(page.body).to include("Edit Fillup")
+      end
+    end
+
+    context 'logged out' do
+      it 'does not let a user view a tweet' do
+        user = User.create(:username => "becky567", :email => "starz@aol.com", :password => "kittens")
+        car = Car.create(:year => 2000, :car_make => "Toyota", :car_model => "Land Cruiser", :nickname => "Land Cruiser", :mileage => 318150, :user_id => user.id)
+        fillup = Fillup.create(:mileage => 215000, :gallons => 12, :octane => "91", :price => 36.00, :brand => "Chevron", :location => "Evergreen, CO", :car_id => car.id, :fillup_date => Time.now)
+
+        get "/fillups/#{fillup.id}"
+        expect(last_response.location).to include("/login")
+      end
+    end
+
+  end
+end
